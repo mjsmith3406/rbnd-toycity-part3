27a0cde23375f5229d7153f5ba54a4dbd8207a2c
mjsmith3406 <mjsmith3406@outlook.com>
2016-06-30 12:20:48 -0500

    Added empty methods to add to later

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,28 @@
 class Product
+  attr_reader :title, :price, :stock, :in_stock
+  @products_in_stock = Product.in_stock
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
   def self.all
     @@products
+  end
+  def find_by_title
+    #find item by title
+  end
+  def find_by_name
+    #find by name
+  end
+  def products_in_stock
+    #must have all instock with more than 0 in_stock
+  end
+  private
+
+  def add_to_products
+    #need more
+    @@products << self
   end
 end
